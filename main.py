0895d3d823a9a28c7456a4813b32ad4db609dbb6
LukasAppleFan <41242166+LukasAppleFan@users.noreply.github.com>
2021-10-02 19:24:03 +0200

    Added some print statements

--- main.py
+++ main.py
@@ -12,8 +12,15 @@ def multiply_two_numbers(a, b,):
 character_name = "John"
 character_age = 35
 is_male = True
+
 print("There once was a man named " + character_name)
 print("He was " + str(character_age))
 
 if is_male:
     print("He is a man")
+
+print("Giraffe\n\"Academy\"")
+phrase = "Phrase"
+print(phrase)
+print(str(add_two_numbers(5, 6) // multiply_two_numbers(1, 3)))
+print(phrase + " c'est cool.")
