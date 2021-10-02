6516ed78dcf3a975079cd9cd57f64c2222247381
LukasAppleFan <41242166+LukasAppleFan@users.noreply.github.com>
2021-10-02 19:02:50 +0200

    Added README.md

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ def multiply_two_numbers(a, b,):
     return a * b
 
 
-print(add_two_numbers(1, 2))
-print("l")
+# print(add_two_numbers(1, 2))
+# print("l")
+
+character_name = "John"
+character_age = 35
+is_male = True
+print("There once was a man named " + character_name)
+print("He was " + str(character_age))
+
+if is_male:
+    print("He is a man")
